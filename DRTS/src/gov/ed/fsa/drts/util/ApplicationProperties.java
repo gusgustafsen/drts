49ba93d8f8fddf401b9fba3ad1c69e3ab6abe670
Barnet Malkin <barnetm@gmail.com>
2016-11-09 19:37:37 -0500

    update some config files

--- DRTS/src/gov/ed/fsa/drts/util/ApplicationProperties.java
+++ DRTS/src/gov/ed/fsa/drts/util/ApplicationProperties.java
@@ -13,359 +13,359 @@ import java.util.ResourceBundle;
 public enum ApplicationProperties {
 
 	SHIRO_PERMISSIONS_BY_GROUP(true, true),
-	
+
 	PROCESS_ID_DATA_REQUEST(true, false),
-	
+
 	DATE_FORMATS(true, true),
-	
+
 	DATE_TIME_FORMATS(true, true),
-	
+
 	ORACLE_URL(true, false),
-	
+
 	ORACLE_USER(true, false),
-	
+
 	ORACLE_PASSWORD(true, false),
-	
+
 	ORACLE_CONNECTION_TYPE(true, false),
-	
+
 	ORACLE_CONNECTION_TYPE_DIRECT(true, false),
-	
+
 	ORACLE_CONNECTION_TYPE_JNDI(true, false),
-	
+
 	ORACLE_JNDI(true, false),
-	
+
 	GROUP_ADMIN(true, false),
-	
+
 	GROUP_SME(true, false),
-	
+
 	GROUP_DRT(true, false),
-	
+
 	GROUP_REPORTER(true, false),
-	
+
 	GROUP_REQUESTOR(true, false),
-	
+
 	GROUP_TYPE_SECURITY(true, false),
-	
+
 	GROUP_TYPE_GENERAL(true, false),
-	
+
 	HOME_PAGE_ADMIN(true, false),
-	
+
 	HOME_PAGE_SME(true, false),
-	
+
 	HOME_PAGE_DRT(true, false),
-	
+
 	HOME_PAGE_REPORTER(true, false),
-	
+
 	HOME_PAGE_REQUESTOR(true, false),
-	
+
 	PAGE_ERROR_APPLICATION(true, false),
-	
+
 	PAGE_ERROR_UNAUTHORIZED(true, false),
-	
+
 	USER_HEADER(true, false),
-	
+
 	USER_SESSION_HEADER(true, false),
-	
+
 	CONTEXT_ROOT(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_DRT_TO(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_DRT_CC(false, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_DRT_FROM(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_DRT_SUBJECT(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_DRT_CONTENT(true, false),
-	
+
 	EMAIL_NOTIFY_ADMIN_DRT_CC(false, false),
-	
+
 	EMAIL_NOTIFY_ADMIN_DRT_FROM(true, false),
-	
+
 	EMAIL_NOTIFY_ADMIN_DRT_SUBJECT(true, false),
-	
+
 	EMAIL_NOTIFY_ADMIN_DRT_CONTENT(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_REQUESTOR_TO(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_REQUESTOR_CC(false, false),
-	
+
 	EMAIL_LABEL_NOTIFY_REQUESTOR_FROM(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_REQUESTOR_SUBJECT(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_REQUESTOR_CONTENT(true, false),
-	
+
 	EMAIL_NOTIFY_REQUESTOR_CC(false, false),
-	
+
 	EMAIL_NOTIFY_REQUESTOR_FROM(true, false),
-	
+
 	EMAIL_NOTIFY_REQUESTOR_SUBJECT(true, false),
-	
+
 	EMAIL_NOTIFY_REQUESTOR_CONTENT(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_SME_NEW_REQUEST_TO(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_SME_NEW_REQUEST_CC(false, false),
-	
+
 	EMAIL_LABEL_NOTIFY_SME_NEW_REQUEST_FROM(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_SME_NEW_REQUEST_SUBJECT(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_SME_NEW_REQUEST_CONTENT(true, false),
-	
+
 	EMAIL_NOTIFY_SME_NEW_REQUEST_CC(false, false),
-	
+
 	EMAIL_NOTIFY_SME_NEW_REQUEST_FROM(true, false),
-	
+
 	EMAIL_NOTIFY_SME_NEW_REQUEST_SUBJECT(true, false),
-	
+
 	EMAIL_NOTIFY_SME_NEW_REQUEST_CONTENT(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_REQUEST_RESOLVED_TO(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_REQUEST_RESOLVED_CC(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_REQUEST_RESOLVED_FROM(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_REQUEST_RESOLVED_SUBJECT(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_REQUEST_RESOLVED_CONTENT(true, false),
-	
+
 	EMAIL_NOTIFY_ADMIN_REQUEST_RESOLVED_CC(true, false),
-	
+
 	EMAIL_NOTIFY_ADMIN_REQUEST_RESOLVED_FROM(true, false),
-	
+
 	EMAIL_NOTIFY_ADMIN_REQUEST_RESOLVED_SUBJECT(true, false),
-	
+
 	EMAIL_NOTIFY_ADMIN_REQUEST_RESOLVED_CONTENT(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_VALIDATOR_TO(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_VALIDATOR_CC(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_VALIDATOR_FROM(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_VALIDATOR_SUBJECT(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_VALIDATOR_CONTENT(true, false),
-	
+
 	EMAIL_NOTIFY_VALIDATOR_CC(true, false),
-	
+
 	EMAIL_NOTIFY_VALIDATOR_FROM(true, false),
-	
+
 	EMAIL_NOTIFY_VALIDATOR_SUBJECT(true, false),
-	
+
 	EMAIL_NOTIFY_VALIDATOR_CONTENT(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_REQUEST_PENDING_APPROVAL_TO(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_REQUEST_PENDING_APPROVAL_CC(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_REQUEST_PENDING_APPROVAL_FROM(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_REQUEST_PENDING_APPROVAL_SUBJECT(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_REQUEST_PENDING_APPROVAL_CONTENT(true, false),
-	
+
 	EMAIL_NOTIFY_REQUEST_PENDING_APPROVAL_CC(true, false),
-	
+
 	EMAIL_NOTIFY_REQUEST_PENDING_APPROVAL_FROM(true, false),
-	
+
 	EMAIL_NOTIFY_REQUEST_PENDING_APPROVAL_SUBJECT(true, false),
-	
+
 	EMAIL_NOTIFY_REQUEST_PENDING_APPROVAL_CONTENT(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_REQUEST_CLOSED_TO(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_REQUEST_CLOSED_CC(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_REQUEST_CLOSED_FROM(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_REQUEST_CLOSED_SUBJECT(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_REQUEST_CLOSED_CONTENT(true, false),
-	
+
 	EMAIL_NOTIFY_REQUEST_CLOSED_CC(true, false),
-	
+
 	EMAIL_NOTIFY_REQUEST_CLOSED_FROM(true, false),
-	
+
 	EMAIL_NOTIFY_REQUEST_CLOSED_SUBJECT(true, false),
-	
+
 	EMAIL_NOTIFY_REQUEST_CLOSED_CONTENT(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_REQUEST_VALIDATED_TO(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_REQUEST_VALIDATED_CC(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_REQUEST_VALIDATED_FROM(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_REQUEST_VALIDATED_SUBJECT(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_REQUEST_VALIDATED_CONTENT(true, false),
-	
+
 	EMAIL_NOTIFY_ADMIN_REQUEST_VALIDATED_CC(true, false),
-	
+
 	EMAIL_NOTIFY_ADMIN_REQUEST_VALIDATED_FROM(true, false),
-	
+
 	EMAIL_NOTIFY_ADMIN_REQUEST_VALIDATED_SUBJECT(true, false),
-	
+
 	EMAIL_NOTIFY_ADMIN_REQUEST_VALIDATED_CONTENT(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_VALIDATION_REJECTED_TO(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_VALIDATION_REJECTED_CC(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_VALIDATION_REJECTED_FROM(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_VALIDATION_REJECTED_SUBJECT(true, false),
-	
+
 	EMAIL_LABEL_NOTIFY_ADMIN_VALIDATION_REJECTED_CONTENT(true, false),
-	
+
 	EMAIL_NOTIFY_ADMIN_VALIDATION_REJECTED_CC(true, false),
-	
+
 	EMAIL_NOTIFY_ADMIN_VALIDATION_REJECTED_FROM(true, false),
-	
+
 	EMAIL_NOTIFY_ADMIN_VALIDATION_REJECTED_SUBJECT(true, false),
-	
+
 	EMAIL_NOTIFY_ADMIN_VALIDATION_REJECTED_CONTENT(true, false),
-	
+
 	DATA_REQUEST_TABLE(true, false),
-	
+
 	DATA_ATTACHMENT_TABLE(true, false),
-	
+
 	DATA_ITERATION_TABLE(true, false),
-	
+
 	DATA_AUDIT_TABLE(true, false),
-	
+
 	DATA_REQUEST_VIEW(true, false),
-	
+
 	DATA_REQUEST_FIELD_ID(true, false),
-	
+
 	DATA_REQUEST_FIELD_ITERATION(true, false),
-	
+
 	DATA_REQUEST_FIELD_DUE_DATE(true, false),
-	
+
 	DATA_REQUEST_FIELD_RELATED_REQUESTS(true, false),
-	
+
 	DATA_REQUEST_FIELD_TOPIC_KEYWORDS(true, false),
-	
+
 	DATA_REQUEST_FIELD_PURPOSE(true, false),
-	
+
 	DATA_REQUEST_FIELD_SPECIAL_CONSIDERATIONS_ISSUES(true, false),
-	
+
 	DATA_REQUEST_FIELD_TYPE(true, false),
-	
+
 	DATA_REQUEST_FIELD_STATUS(true, false),
-	
+
 	DATA_REQUEST_FIELD_CREATED_DATE_TIME(true, false),
-	
+
 	DATA_REQUEST_FIELD_CREATED_BY(true, false),
-	
+
 	DATA_REQUEST_FIELD_DESCRIPTION(true, false),
-	
+
 	DATA_REQUEST_FIELD_URGENT(true, false),
-	
+
 	DATA_REQUEST_FIELD_REQUESTOR_NAME(true, false),
-	
+
 	DATA_REQUEST_FIELD_REQUESTOR_ORGANIZATION(true, false),
-	
+
 	DATA_REQUEST_FIELD_REQUESTOR_PHONE(true, false),
-	
+
 	DATA_REQUEST_FIELD_REQUESTOR_EMAIL(true, false),
-	
+
 	DATA_REQUEST_FIELD_RECEIVER_NAME(true, false),
-	
+
 	DATA_REQUEST_FIELD_RECEIVER_EMAIL(true, false),
-	
+
 	DATA_REQUEST_FIELD_ASSIGNED_SME(true, false),
-	
+
 	DATA_REQUEST_FIELD_ASSIGNED_SME_FULL_NAME(true, false),
 
 	DATA_REQUEST_FIELD_ASSIGNED_TO_SME(true, false),
-	
+
 	DATA_REQUEST_FIELD_DATE_RESOLVED(true, false),
-	
+
 	DATA_REQUEST_FIELD_ASSIGNED_VALIDATOR(true, false),
-	
+
 	DATA_REQUEST_FIELD_ASSIGNED_TO_VALIDATOR(true, false),
-	
+
 	DATA_REQUEST_FIELD_DATE_VALIDATED(true, false),
-	
+
 	DATA_REQUEST_FIELD_DATE_CLOSED(true, false),
-	
+
 	DATA_REQUEST_FIELD_COMMENTS(true, false),
-	
+
 	DATA_REQUEST_FIELD_LAST_UPDATED_DATE(true, false),
-	
+
 	DATA_REQUEST_FIELD_PII_FLAG(true, false),
-	
+
 	DATA_REQUEST_FIELD_PROCESS_INSTANCE_ID(true, false),
-	
+
 	DATA_REQUEST_FIELD_CANDIDATE_GROUP(true, false),
-	
+
 	DATA_REQUEST_FIELD_ASSIGNEE(true, false),
-	
+
 	DATA_REQUEST_FIELD_SYSTEM(true, false),
-	
+
 	DATA_REQUEST_FIELD_DELAY_REASON(true, false),
-	
+
 	DATA_REQUEST_FIELD_ORIGINAL_REQUEST_DATE(true, false),
-	
+
 	DATA_REQUEST_FIELD_AGREED_DUE_DATE(true, false),
-	
+
 	DATA_REQUEST_FIELD_ANTICIPATED_DUE_DATE(true, false),
-	
+
 	DATA_REQUEST_FIELD_DATE_RUN(true, false),
-	
+
 	DATA_REQUEST_FIELD_REPORT_TYPE(true, false),
-	
+
 	DATA_REQUEST_FIELD_QUERY_REPORT_NAME(true, false),
-	
+
 	DATA_REQUEST_FIELD_CLARIFICATIONS_ASSUMPTIONS(true, false),
-	
+
 	DATA_REQUEST_FIELD_DETAILED_STEPS(true, false),
-	
+
 	DATA_REQUEST_FIELD_VALIDATION_DESCRIPTION(true, false),
-	
+
 	DATA_REQUEST_FIELD_VALIDATION_RESULT(true, false),
-	
+
 	DATA_REQUEST_FIELD_GOLDEN_QUERY_LIBRARY(true, false),
-	
+
 	DATA_REQUEST_FIELD_BUSINESS_REQUIREMENTS(true, false),
-	
+
 	DATA_REQUEST_FIELD_TIER(true, false),
-	
+
 	DATA_REQUEST_FIELD_SME_GROUP(true, false),
-	
+
 	DATA_REQUEST_FIELD_VALIDATION_SME_GROUP(true, false),
-	
+
 	DATA_REQUEST_WORKFLOW_REQUEST_REJECTED_BY_ADMIN(true, false),
-	
+
 	DATA_REQUEST_WORKFLOW_REQUEST_REJECTED_BY_SME(true, false),
-	
+
 	DATA_REQUEST_WORKFLOW_REQUEST_DRAFTED(true, false),
-	
+
 	DATA_REQUEST_WORKFLOW_REQUEST_ASSIGNED_TO_VALIDATOR(true, false),
-	
+
 	DATA_REQUEST_WORKFLOW_REQUEST_VALIDATED(true, false),
-	
+
 	DATA_REQUEST_WORKFLOW_VALIDATED_REQUEST_CLOSED(true, false),
-	
+
 	DATA_REQUEST_WORKFLOW_REQUEST_DRAFTED_SUBMITTED(true, false),
-	
+
 	DATA_REQUEST_STATUS_DRAFTED(true, false),
-	
+
 	DATA_REQUEST_STATUS_PENDING(true, false),
-	
+
 	DATA_REQUEST_STATUS_ASSIGNED_TO_SME(true, false),
-	
+
 	DATA_REQUEST_STATUS_RESOLVED(true, false),
-	
+
 	DATA_REQUEST_STATUS_REJECTED_BY_ADMIN(true, false),
-	
+
 	DATA_REQUEST_STATUS_REJECTED_BY_SME(true, false),
-	
+
 	DATA_REQUEST_STATUS_ASSIGNED_TO_VALIDATOR(true, false),
 
 	DATA_REQUEST_STATUS_PENDING_REQUESTOR_APPROVAL(true, false),
@@ -379,84 +379,83 @@ public enum ApplicationProperties {
 	DATA_REQUEST_STATUS_VALIDATION_REJECTED(true, false),
 
 	DATA_REQUEST_STATUS_DISCARDED(true, false),
-	
+
 	DATA_REQUEST_TYPES(true, true),
-	
+
 	DATA_REQUEST_SYSTEMS(true, true),
-	
+
 	DATA_REQUEST_DELAY_REASONS(true, true),
-	
+
 	DATA_REQUEST_REPORT_TYPES(true, true),
-	
+
 	DATA_REQUEST_SME_GROUPS(true, true),
-	
+
 	DATA_REQUEST_TIERS(true, true),
-	
+
 	ATTACHMENT_FIELD_ID(true, false),
-	
+
 	ATTACHMENT_FIELD_REQUEST_ID(true, false),
-	
+
 	ATTACHMENT_FIELD_FILE_NAME(true, false),
-	
+
 	ATTACHMENT_FIELD_FILE_TYPE(true, false),
-	
+
 	ATTACHMENT_FIELD_FILE_SIZE(true, false),
-	
+
 	ATTACHMENT_FIELD_FILE_CONTENT(true, false),
-	
+
 	ITERATION_FIELD_PARENT_ID(true, false),
-	
+
 	ITERATION_FIELD_ITERATION(true, false),
-	
+
 	ITERATION_FIELD_CHILD_ID(true, false),
-	
+
 	AUDIT_FIELD_ID(true, false),
-	
+
 	AUDIT_FIELD_REQUEST_NUMBER(true, false),
 
 	AUDIT_FIELD_FIELD_NAME(true, false),
-	
+
 	AUDIT_FIELD_OLD_VALUE(true, false),
-	
+
 	AUDIT_FIELD_NEW_VALUE(true, false),
 
 	AUDIT_FIELD_MODIFIED_DATE(true, false),
-	
-	AUDIT_FIELD_MODIFIED_BY(true, false);
-	
+
+	AUDIT_FIELD_MODIFIED_BY(true, false),
+
+	USER_HEADER_OVERRIDE(true, false),
+
+	USER_HEADER_OVERRIDE_VALUE(false, false);
+
 	private static final String LIST_DELIMITER = "\\|";
-	
+
 	private String string_value;
 	private List<String> list_value;
-	
+
 	private Properties properties = null;
 
 	private static final String SYSTEM_PROPERTIES_PREFIX = "gov.ed.fsa.drts.";
 
 	public static final String PROPERTIES_FILE_NAME = "drts";
-	
-	private ApplicationProperties()
-	{
+
+	private ApplicationProperties() {
 		this(true, false);
 	}
-	
-	private ApplicationProperties(final boolean required)
-	{
+
+	private ApplicationProperties(final boolean required) {
 		this(required, false);
 	}
-	
-	private ApplicationProperties(final boolean required, final boolean list_format)
-	{
-		if(properties == null)
-		{
+
+	private ApplicationProperties(final boolean required, final boolean list_format) {
+		if (properties == null) {
 			properties = new Properties();
 
 			final ResourceBundle bundle = ResourceBundle.getBundle(PROPERTIES_FILE_NAME);
 
 			final Iterator<String> bundle_iterator = bundle.keySet().iterator();
 
-			while(bundle_iterator.hasNext())
-			{
+			while (bundle_iterator.hasNext()) {
 				final String key = bundle_iterator.next();
 				final String value = bundle.getString(key);
 				properties.setProperty(key.toLowerCase().replace('.', '_'), value);
@@ -466,66 +465,57 @@ public enum ApplicationProperties {
 
 			final Iterator<Entry<Object, Object>> system_properties_iterator = system_properties.entrySet().iterator();
 
-			while(system_properties_iterator.hasNext())
-			{
+			while (system_properties_iterator.hasNext()) {
 				final Entry<Object, Object> entry = system_properties_iterator.next();
 
 				final String key = (String) entry.getKey();
 				final String value = (String) entry.getValue();
 
-				if(key.startsWith(SYSTEM_PROPERTIES_PREFIX) && !key.equals(SYSTEM_PROPERTIES_PREFIX))
-				{
-					final String key_to_use = key.substring(SYSTEM_PROPERTIES_PREFIX.length()).toLowerCase().replace('.', '_');
+				if (key.startsWith(SYSTEM_PROPERTIES_PREFIX) && !key.equals(SYSTEM_PROPERTIES_PREFIX)) {
+					final String key_to_use = key.substring(SYSTEM_PROPERTIES_PREFIX.length()).toLowerCase()
+							.replace('.', '_');
 					properties.setProperty(key_to_use, value);
 				}
 			}
 		}
-	
+
 		string_value = properties.getProperty(toString().toLowerCase());
 
-		if(string_value != null && string_value.isEmpty())
-		{
+		if (string_value != null && string_value.isEmpty()) {
 			string_value = null;
 		}
 
-		if(string_value == null)
-		{
+		if (string_value == null) {
 			list_value = Collections.emptyList();
-			
-			if(required)
-			{
-				throw new MissingResourceException("Property '" + toString() + "' not found in resource bundle '" + ApplicationProperties.PROPERTIES_FILE_NAME + "', required", ApplicationProperties.PROPERTIES_FILE_NAME, toString());
+
+			if (required) {
+				throw new MissingResourceException(
+						"Property '" + toString() + "' not found in resource bundle '"
+								+ ApplicationProperties.PROPERTIES_FILE_NAME + "', required",
+						ApplicationProperties.PROPERTIES_FILE_NAME, toString());
 			}
-		}
-		else
-		{
+		} else {
 			string_value = string_value.trim();
 
 			list_value = new ArrayList<String>();
-			
-			if(list_format)
-			{
+
+			if (list_format) {
 				list_value.addAll(Arrays.asList(string_value.split(LIST_DELIMITER)));
-			}
-			else
-			{
+			} else {
 				list_value.add(string_value);
 			}
 		}
 	}
-	
-	public String getStringValue()
-	{
+
+	public String getStringValue() {
 		return string_value;
 	}
 
-	public boolean getBooleanValue()
-	{
+	public boolean getBooleanValue() {
 		return Boolean.parseBoolean(string_value);
 	}
 
-	public List<String> getListValue()
-	{
+	public List<String> getListValue() {
 		return list_value;
 	}
 }
--- DRTS/src/gov/ed/fsa/drts/util/RequestValidator.java
+++ DRTS/src/gov/ed/fsa/drts/util/RequestValidator.java
@@ -7,9 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import gov.ed.fsa.drts.bean.UserSession;
-import gov.ed.fsa.drts.security.DRTSRealm;
-
 import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
@@ -26,31 +23,37 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.subject.Subject;
 
+import gov.ed.fsa.drts.bean.UserSession;
+import gov.ed.fsa.drts.security.DRTSRealm;
+
 public class RequestValidator implements PhaseListener {
 
 	private static final long serialVersionUID = -2282664829406618457L;
-	
+
 	private static final Logger logger = Logger.getLogger(RequestValidator.class);
 
 	@Override
-	public void afterPhase(PhaseEvent phase_event){}
+	public void afterPhase(PhaseEvent phase_event) {
+	}
 
 	@Override
-	public void beforePhase(PhaseEvent phase_event) 
-	{
+	public void beforePhase(PhaseEvent phase_event) {
 		FacesContext faces_context = phase_event.getFacesContext();
-		
+
 		HttpServletResponse http_response = (HttpServletResponse) faces_context.getExternalContext().getResponse();
 		HttpServletRequest http_request = (HttpServletRequest) faces_context.getExternalContext().getRequest();
-		
+
 		String request_url = http_request.getRequestURL().toString();
-		
+
 		Map<String, String> request_header_map = faces_context.getExternalContext().getRequestHeaderMap();
-		
-		final String incoming_user = request_header_map.get(ApplicationProperties.USER_HEADER.getStringValue());
-		
-		UserSession temp_user_session = (UserSession) faces_context.getExternalContext().getSessionMap().get(ApplicationProperties.USER_SESSION_HEADER.getStringValue());
-		
+
+		final String incoming_user = ApplicationProperties.USER_HEADER_OVERRIDE.getBooleanValue()
+				? ApplicationProperties.USER_HEADER_OVERRIDE_VALUE.getStringValue()
+				: request_header_map.get(ApplicationProperties.USER_HEADER.getStringValue());
+
+		UserSession temp_user_session = (UserSession) faces_context.getExternalContext().getSessionMap()
+				.get(ApplicationProperties.USER_SESSION_HEADER.getStringValue());
+
 		String destination_url = null;
 
 		/*
@@ -61,107 +64,91 @@ public class RequestValidator implements PhaseListener {
 		 * page being framed
 		 */
 		http_response.addHeader("X-FRAME-OPTIONS", "SAMEORIGIN");
-		
-		if(request_url.contains("/newRequest") == true)
-		{
+
+		if (request_url.contains("/newRequest") == true) {
 			faces_context.getExternalContext().getFlash().put("drtsDataRequest", null);
 		}
-		
-		if(request_url.contains("/error") == false)
-		{
-			if(Utils.isStringEmpty(incoming_user) == false)
-			{
+
+		if (request_url.contains("/error") == false) {
+			if (Utils.isStringEmpty(incoming_user) == false) {
 				logger.debug("user header: " + incoming_user);
-				
-				if(temp_user_session == null)
-				{
+
+				if (temp_user_session == null) {
 					logger.info("user session is null");
-					
+
 					IdentityService identity_service = ProcessEngines.getDefaultProcessEngine().getIdentityService();
-					
-					if(identity_service != null)
-					{
+
+					if (identity_service != null) {
 						User activiti_user = identity_service.createUserQuery().userId(incoming_user).singleResult();
-						
-						if(activiti_user != null)
-						{
-							List<Group> security_groups = identity_service.createGroupQuery().groupType(ApplicationProperties.GROUP_TYPE_SECURITY.getStringValue()).groupMember(activiti_user.getId()).list();
-							
+
+						if (activiti_user != null) {
+							List<Group> security_groups = identity_service.createGroupQuery()
+									.groupType(ApplicationProperties.GROUP_TYPE_SECURITY.getStringValue())
+									.groupMember(activiti_user.getId()).list();
+
 							List<String> string_groups = new ArrayList<String>();
-							
-							for(Group group : security_groups)
-							{
+
+							for (Group group : security_groups) {
 								string_groups.add(group.getName().toLowerCase());
 							}
-							
-							if(security_groups != null && security_groups.size() > 0)
-							{
+
+							if (security_groups != null && security_groups.size() > 0) {
 								Set<String> roles = new HashSet<String>(string_groups);
-								
+
 								final DRTSRealm realm = new DRTSRealm(roles);
-								
+
 								final DefaultSecurityManager securityManager = new DefaultSecurityManager(realm);
-								
+
 								Subject subject = new Subject.Builder(securityManager).buildSubject();
-								
-								subject.login(new UsernamePasswordToken(DRTSRealm.DEFAULT_USERNAME, DRTSRealm.DEFAULT_PASSWORD));
-								
+
+								subject.login(new UsernamePasswordToken(DRTSRealm.DEFAULT_USERNAME,
+										DRTSRealm.DEFAULT_PASSWORD));
+
 								UserSession user_session = new UserSession();
 								user_session.setUser(activiti_user);
 								user_session.setSubject(subject);
-								
-								faces_context.getExternalContext().getSessionMap().put(ApplicationProperties.USER_SESSION_HEADER.getStringValue(), user_session);
-								
+
+								faces_context.getExternalContext().getSessionMap()
+										.put(ApplicationProperties.USER_SESSION_HEADER.getStringValue(), user_session);
+
 								logger.info("user session created");
-								
-								if(request_url.contains("index.htm") == true)
-								{
-									destination_url = ApplicationProperties.CONTEXT_ROOT.getStringValue() + user_session.getHomePage();
+
+								if (request_url.contains("index.htm") == true) {
+									destination_url = ApplicationProperties.CONTEXT_ROOT.getStringValue()
+											+ user_session.getHomePage();
 								}
-							}
-							else
-							{
+							} else {
 								logger.error("User does not have any groups.");
-								destination_url = ApplicationProperties.CONTEXT_ROOT.getStringValue() + ApplicationProperties.PAGE_ERROR_UNAUTHORIZED.getStringValue();
+								destination_url = ApplicationProperties.CONTEXT_ROOT.getStringValue()
+										+ ApplicationProperties.PAGE_ERROR_UNAUTHORIZED.getStringValue();
 							}
-						}
-						else
-						{
+						} else {
 							logger.error("User was not found in Activiti.");
-							destination_url = ApplicationProperties.CONTEXT_ROOT.getStringValue() + ApplicationProperties.PAGE_ERROR_UNAUTHORIZED.getStringValue();
+							destination_url = ApplicationProperties.CONTEXT_ROOT.getStringValue()
+									+ ApplicationProperties.PAGE_ERROR_UNAUTHORIZED.getStringValue();
 						}
-					}
-					else
-					{
+					} else {
 						logger.error("Unable to create an Identity Service.");
-						destination_url = ApplicationProperties.CONTEXT_ROOT.getStringValue() + ApplicationProperties.PAGE_ERROR_APPLICATION.getStringValue();
+						destination_url = ApplicationProperties.CONTEXT_ROOT.getStringValue()
+								+ ApplicationProperties.PAGE_ERROR_APPLICATION.getStringValue();
 					}
-				}
-				else
-				{
+				} else {
 					logger.debug("user session already exists.");
 				}
-			}
-			else
-			{
+			} else {
 				logger.error("User header is empty.");
-				destination_url = ApplicationProperties.CONTEXT_ROOT.getStringValue() + ApplicationProperties.PAGE_ERROR_UNAUTHORIZED.getStringValue();
+				destination_url = ApplicationProperties.CONTEXT_ROOT.getStringValue()
+						+ ApplicationProperties.PAGE_ERROR_UNAUTHORIZED.getStringValue();
 			}
-			
-			if(destination_url != null)
-			{
+
+			if (destination_url != null) {
 				logger.info("Redirecting to: " + destination_url);
-				
-				try
-				{
+
+				try {
 					faces_context.getExternalContext().redirect(destination_url);
-				}
-				catch(IOException ioe)
-				{
+				} catch (IOException ioe) {
 					logger.error("Unable redirect to appropriate page.", ioe);
-				}
-				catch(Exception e)
-				{
+				} catch (Exception e) {
 					logger.error("Unable redirect to appropriate page.", e);
 				}
 			}
@@ -169,8 +156,7 @@ public class RequestValidator implements PhaseListener {
 	}
 
 	@Override
-	public PhaseId getPhaseId()
-	{
+	public PhaseId getPhaseId() {
 		return PhaseId.RESTORE_VIEW;
 	}
 }
