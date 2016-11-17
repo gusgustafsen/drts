dd846a639c5906433981c805a87bb300ac662f22
Barnet Malkin <barnetm@gmail.com>
2016-11-16 22:17:07 -0500

    upload file works

--- DRTS/WebContent/resources/js/main.js
+++ DRTS/WebContent/resources/js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
     });
 	
 	$("#fileuploader").uploadFile({
-		url: "/drts/file",
+		url: "../file",
 		multiple: true,
 		fileName: "myfile",
 		showStatusAfterSuccess: false,
@@ -34,7 +34,7 @@ $(document).ready(function(){
 		},
 		onSuccess:function(files, data, xhr)
 		{
-			var html_addition = '<span class="form-text">' + data.uploaded_file_name + ' <a href="/drts/file?file_id=' + data.uploaded_file_id + '" target="_blank">View</a></span>';
+			var html_addition = '<span class="form-text">' + data.uploaded_file_name + ' <a href="../file?file_id=' + data.uploaded_file_id + '" target="_blank">View</a></span>';
 			$("#no-file").remove();
 			$('#attachments').html($('#attachments').html() + html_addition);
 		}
