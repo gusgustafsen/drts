076c54b1e98c1d66cbd3036d0b35ef667d3c8475
Barnet Malkin <barnetm@gmail.com>
2017-02-11 22:11:56 -0500

    created drts/aims integration web service client

---
+++ DRTS/src/gov/ed/fsa/drts/AimsDrtsService/AimsDrtsService_PortType.java
@@ -0,0 +1,13 @@
+/**
+ * AimsDrtsService_PortType.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package gov.ed.fsa.drts.AimsDrtsService;
+
+public interface AimsDrtsService_PortType extends java.rmi.Remote {
+    public gov.ed.fsa.drts.AimsDrts.CreateAccountResponseType createAccount(gov.ed.fsa.drts.AimsDrts.CreateAccountRequestType createAccountRequest) throws java.rmi.RemoteException;
+    public gov.ed.fsa.drts.AimsDrts.DeleteAccountResponseType deleteAccount(gov.ed.fsa.drts.AimsDrts.DeleteAccountRequestType deleteAccountRequest) throws java.rmi.RemoteException;
+}
---
+++ DRTS/src/gov/ed/fsa/drts/AimsDrtsService/AimsDrtsService_Service.java
@@ -0,0 +1,16 @@
+/**
+ * AimsDrtsService_Service.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package gov.ed.fsa.drts.AimsDrtsService;
+
+public interface AimsDrtsService_Service extends javax.xml.rpc.Service {
+    public java.lang.String getAimsDrtsServiceSOAPAddress();
+
+    public gov.ed.fsa.drts.AimsDrtsService.AimsDrtsService_PortType getAimsDrtsServiceSOAP() throws javax.xml.rpc.ServiceException;
+
+    public gov.ed.fsa.drts.AimsDrtsService.AimsDrtsService_PortType getAimsDrtsServiceSOAP(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
